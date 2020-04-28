a7f14e4974c276de899a854352b26704c0e1b2b5
hshine1226 <hshine1226@gmail.com>
2020-04-29 00:42:25 +0900

    버그: Finished 버튼 문제 수정 Finished에서 버튼이 동작 안하는 문제 수정. 원인은 두 개의 함수를 돌아가면서 씀. 해결

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ let finished = [];
 function saveToDos() {
   localStorage.setItem(PENDING, JSON.stringify(toDos));
 }
-
+function saveFinish() {
+  localStorage.setItem(FINISHED, JSON.stringify(finished));
+}
 function handleDelete(event) {
   const button = event.target;
   const li = button.parentNode;
@@ -27,31 +29,6 @@ function handleDelete(event) {
   });
   toDos = cleanToDos;
   saveToDos();
-}
-function saveFinish() {
-  localStorage.setItem(FINISHED, JSON.stringify(finished));
-}
-function paintFinish(text) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerText = text;
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "Delete";
-  const pendingBtn = document.createElement("button");
-  const newId = finished.length + 1;
-  li.id = newId;
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.appendChild(pendingBtn);
-  pendingBtn.innerText = "Pending";
-  finishedList.appendChild(li);
-
-  const finishObj = {
-    id: newId,
-    text: text,
-  };
-  finished.push(finishObj);
-  saveFinish();
 }
 function handleFinish(event) {
   const button = event.target;
@@ -66,7 +43,69 @@ function handleFinish(event) {
   toDos = cleanToDos;
   saveToDos();
 
-  paintFinish(text);
+  paintFinishedList(text);
+}
+function handlePending(event) {
+  const button = event.target;
+  const li = button.parentNode;
+  const span = li.children[0];
+
+  const text = span.innerText;
+
+  finishedList.removeChild(li);
+
+  paintPendingList(text);
+
+  const cleanFinish = finished.filter(function (finish) {
+    return finish.id !== parseInt(li.id);
+  });
+  finished = cleanFinish;
+  saveFinish();
+}
+function handleInputChange(event) {
+  const currentValue = input.value;
+  paintPendingList(currentValue);
+}
+function handleFinishDelete(event) {
+  console.log("inin");
+  const button = event.target;
+  const li = button.parentNode;
+  finishedList.removeChild(li);
+
+  const cleanFinish = finished.filter(function (finish) {
+    return finish.id !== parseInt(li.id);
+  });
+  finished = cleanFinish;
+  saveFinish();
+}
+input.addEventListener("change", handleInputChange);
+
+function paintFinishedList(text) {
+  console.log("inPaint");
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = text;
+  const finishDelBtn = document.createElement("button");
+  finishDelBtn.innerText = "Delete";
+  const pendingBtn = document.createElement("button");
+  pendingBtn.innerText = "Pending";
+  const newId = finished.length + 1;
+  li.id = newId;
+  li.appendChild(span);
+  li.appendChild(finishDelBtn);
+  li.appendChild(pendingBtn);
+  pendingBtn.innerText = "Pending";
+  finishedList.appendChild(li);
+
+  finishDelBtn.addEventListener("click", handleFinishDelete);
+  pendingBtn.addEventListener("click", handlePending);
+
+  const finishObj = {
+    id: newId,
+    text: text,
+  };
+  finished.push(finishObj);
+  saveFinish();
 }
 function paintPendingList(text) {
   li = document.createElement("li");
@@ -96,10 +135,6 @@ function paintPendingList(text) {
   toDos.push(toDoObj);
   saveToDos();
 }
-function handleInputChange(event) {
-  const currentValue = input.value;
-  paintPendingList(currentValue);
-}
 
 function loadPendingList() {
   const loadedPending = localStorage.getItem(PENDING);
@@ -110,66 +145,6 @@ function loadPendingList() {
     });
   }
 }
-
-input.addEventListener("change", handleInputChange);
-
-function handleFinishDelete(event) {
-  const button = event.target;
-  const li = button.parentNode;
-  finishedList.removeChild(li);
-
-  const cleanFinish = finished.filter(function (finish) {
-    return finish.id !== parseInt(li.id);
-  });
-  finished = cleanFinish;
-  saveFinish();
-}
-
-function handlePending(event) {
-  const button = event.target;
-  const li = button.parentNode;
-  const span = li.children[0];
-
-  const text = span.innerText;
-
-  finishedList.removeChild(li);
-
-  paintPendingList(text);
-
-  const cleanFinish = finished.filter(function (finish) {
-    return finish.id !== parseInt(li.id);
-  });
-  finished = cleanFinish;
-  saveFinish();
-}
-
-function paintFinishedList(text) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerText = text;
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "Delete";
-  const pendingBtn = document.createElement("button");
-  pendingBtn.innerText = "Pending";
-  const newId = finished.length + 1;
-  li.id = newId;
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.appendChild(pendingBtn);
-  pendingBtn.innerText = "Pending";
-  finishedList.appendChild(li);
-
-  delBtn.addEventListener("click", handleFinishDelete);
-  pendingBtn.addEventListener("click", handlePending);
-
-  const finishObj = {
-    id: newId,
-    text: text,
-  };
-  finished.push(finishObj);
-  saveFinish();
-}
-
 function loadFinishedList() {
   const loadedFinished = localStorage.getItem(FINISHED);
   if (loadedFinished !== null) {
@@ -181,7 +156,7 @@ function loadFinishedList() {
 }
 
 function init() {
-  loadPendingList();
   loadFinishedList();
+  loadPendingList();
 }
 init();
